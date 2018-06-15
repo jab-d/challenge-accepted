9a57d9440158ca02a1f8f50d594450b05a27e384
dorianinc <dorianrdba@gmail.com>
2018-06-14 20:45:00 -0700

    Created join table between user table and challenges table by creating acceptComplete table(Joins Table)

--- models/user.js
+++ models/user.js
@@ -18,10 +18,10 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   User.associate = function (models) {
-    // associations can be defined here
     User.hasMany(models.Challenge, {
       onDelete: "cascade"
     });
+    User.belongsToMany(models.Challenge, {through: models.AcceptComplete});    
   };
 
   return User;
