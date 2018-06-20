2f70940c7d3a1f2c3150a46b3e7b346e6e5f1c59
andrewhemans <andrewhemans@gmail.com>
2018-06-19 17:10:02 -0700

    more tests

--- cypress/integration/feed.spec.js
+++ cypress/integration/feed.spec.js
@@ -1,6 +1,21 @@
-describe('My First Test', function() {
+
+beforeEach(function () {
+  cy.viewport(419, 800)
+})
+
+describe('Checks Title', function() {
   it('Should have title', function() {
     cy.visit('http://localhost:8080/feed')
     cy.get('#main-title').should('contain', 'Challenge Accepted')
+  })
+})
+
+describe('Open and close modal', function() {
+  it('Should open and close', function() {
+
+    cy.get('#showModal-1').click()
+    cy.wait(500)
+    cy.get('#close-1').click()
+    cy.get('#modal-1').should('not.have.class', 'is-active')
   })
 })
